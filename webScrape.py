b7fb92f386bfcf1cb6b732cdb84059c63c4e1e6e
Jack Newton <102347457+JackDarkability@users.noreply.github.com>
2024-10-20 03:18:21 +0100

    Full web scraping of all years

--- webScrape.py
+++ webScrape.py
@@ -4,30 +4,38 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-
+from multiprocessing import Pool
 import pandas as pd
 
 
-def get_link(driver, base_url, competition, season, club="-1"):
+def setup_driver():
+    """Set up the web driver"""
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+
+def get_full_link(base_url, competition, season, club="-1"):
     """Create full URL including relevant details and return beautifulsoup object"""
 
-    # a country's league
-
     full_url = f"{base_url}{competition}&se={season['number']}&cl={club}"
-    driver.get(full_url)
-    time.sleep(2)
-    logging.info(full_url)
-    return BeautifulSoup(driver.page_source, "html.parser")
+    logging.debug(full_url)
+    return full_url
 
 
-def get_all_season_data(driver, base_url, competition):
-    """getting all the links for the seasons"""
+def get_all_season_data(base_url, competition):
+    """Getting all the ids and years for the seasons"""
+
     link = f"{base_url}{competition}"
+    # Set up the web driver
+    driver = setup_driver()
+
     driver.get(link)
-    logging.info(link)
+    logging.debug(link)
+
     time.sleep(5)
     soup = BeautifulSoup(driver.page_source, "html.parser")
     seasons_drop_down = soup.find(
@@ -42,27 +50,36 @@ def get_all_season_data(driver, base_url, competition):
         year_of_season = season.text.strip()  # Like "2021/2022"
         season_data.append({"number": id_of_season, "year": year_of_season})
 
+    driver.quit()
+
     return season_data
 
 
-def get_data(soup, driver, league, year, results_of_matches):
-    """Get all mathes from season"""
-    # Div class loader to know if still loading or no.
+def get_matches_data(full_link, league, year):
+    """Get all the matches for a given season"""
 
-    html = driver.find_element(By.TAG_NAME,"html")
+    driver = setup_driver()
+    driver.get(full_link)
+
+    results_of_matches = []
+    html = driver.find_element(By.TAG_NAME, "html")
+    html.send_keys(Keys.PAGE_DOWN)
+    time.sleep(10)
 
     # Scroll down page until all matches are loaded
     while True:
         html.send_keys(Keys.PAGE_DOWN)
+        time.sleep(4)
         soup = BeautifulSoup(driver.page_source, "html.parser")
-        loader = soup.findAll(lambda tag: tag.name == 'div' and 
-                'loader' in tag.get('class', []) and 
-                'u-hide' not in tag.get('class', []))
-        time.sleep(2)
+        # If loader present then matches have not finished loading
+        loader = soup.findAll(
+            lambda tag: tag.name == "div"
+            and "loader" in tag.get("class", [])
+            and "u-hide" not in tag.get("class", [])
+        )
         if not loader:
+            logging.debug("No loader")
             break
-
-
 
     # Get all the matches
     all_match_days = soup.find_all("div", class_="fixtures__date-container")
@@ -71,6 +88,7 @@ def get_data(soup, driver, league, year, results_of_matches):
         date = match_day.find("time", class_="fixtures__date fixtures__date--long").text
         matches = match_day.find_all("li", class_="match-fixture")
         for match in matches:
+            # Get the data for the individual match
             home_team = match.get("data-home")
             away_team = match.get("data-away")
             venue = match.get("data-venue")
@@ -101,6 +119,7 @@ def get_data(soup, driver, league, year, results_of_matches):
                     "date": date,
                 }
             )
+    driver.quit()
 
     return results_of_matches
 
@@ -112,45 +131,44 @@ def main():
     logging_format = "[%(levelname)s] %(message)s"
     logging.basicConfig(level=logging_level, format=logging_format)
 
-    # Set up the web driver
-    options = webdriver.ChromeOptions()
-    #options.add_argument("headless")
-    driver = webdriver.Chrome(options=options)
-
-    # Example URL: https://www.premierleague.com/results?co=1&se=363&cl=-1
-    # co = competition, 1 = Premier League
-    # se = season, 363 = 2021-2022
-    # cl = club, -1 = all clubs
+    """
+    Example URL: https://www.premierleague.com/results?co=1&se=363&cl=-1
+    co = competition, 1 = Premier League
+    se = season, 363 = 2021-2022
+    cl = club, -1 = all clubs
+    """
 
     BASE_URL = "https://www.premierleague.com/results?co="
     COMPETITIONS = ["1"]  # Just premier league for now
     CLUBS = ["-1"]  # All clubs for now
-    seasons = get_all_season_data(driver, BASE_URL, "1")
+    seasons = get_all_season_data(
+        BASE_URL, "1"
+    )  # Only gets season data for premier league for now
 
     for season in seasons:
         logging.info(season["year"] + " " + season["number"])
 
     results_of_matches = []
-    season = seasons[0]
-    for competition in COMPETITIONS:
-        #for season in seasons:
-            for club in CLUBS:
-                logging.info('Testing for Club: %s, Competition: %s, Season: %s' % (club,competition,season))
-                time.sleep(2)
-                soup = get_link(driver, BASE_URL, competition, season, club)
-                results_of_matches = get_data(
-                    soup, driver, competition, season, results_of_matches
-                )
 
-                logging.info('Got data for Club: %s, Competition: %s, Season: %s' % (club,competition,season))
-    
+    # Parallel processing
+    with Pool(processes=2) as pool:
+        tasks = []
+        for competition in COMPETITIONS:
+            for season in seasons:
+                for club in CLUBS:
+                    full_link = get_full_link(BASE_URL, competition, season, club)
+                    tasks.append((full_link, competition, season))
+
+        print(tasks)
+
+        for data in pool.starmap(get_matches_data, tasks):
+            results_of_matches.extend(data)
+
     # Convert all matches to a DataFrame
     all_matches_df = pd.DataFrame(results_of_matches)
 
     # Save to CSV, different encoding due to accented characters
     all_matches_df.to_csv("football_matches.csv", encoding="iso-8859-1", index=False)
-
-    
 
 
 if __name__ == "__main__":
